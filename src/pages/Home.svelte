5e0bf2fb9297a4d44ca3a4f1526a70fc098dbc99
Fogrexon <fogrexon@gmail.com>
2020-09-13 23:55:48 +0900

    テストページ

---
+++ src/pages/Home.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Jumbotron, Container } from 'sveltestrap';
+</script>
+<Jumbotron>
+  <h1>
+    Hello, world!
+  </h1>
+</Jumbotron>
+<Container>
+  <p>
+    これはテストページですよ
+  </p>
+</Container>
