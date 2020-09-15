16c39cab1d540a0ee843a3a7b4c947abc793230b
chikuwa-taso <leoleozawawa@gmail.com>
2020-09-15 20:35:45 +0900

    Weather

---
+++ src/components/Current/Weather.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Media } from 'sveltestrap';
+</script>
+
+<Media>
+  <Media left href="#">
+    <Media
+      object
+      src="http://openweathermap.org/img/wn/10d@2x.png"
+      alt="weather image" />
+  </Media>
+  <Media body>
+    XX℃
+  </Media>
+</Media>
