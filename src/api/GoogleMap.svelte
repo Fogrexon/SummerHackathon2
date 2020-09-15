48b273dd909d19f0465f64b97e157cd5eeabc881
Fogrexon <fogrexon@gmail.com>
2020-09-15 18:27:44 +0900

    googlemap

--- src/api/GoogleMap.svelte
+++ src/api/GoogleMap.svelte
@@ -1,32 +1,71 @@
+
 <script>
-  import { onLocationChange } from '../stores/geoLocationStore';
-  // eslint-disable-next-line import/prefer-default-export
-  export let pins = [];
+/* eslint-disable no-undef */
+import { onLocationChange } from '../stores/geoLocationStore';
+// eslint-disable-next-line import/prefer-default-export
+export let pins = [];
 
-  let container;
-  let map;
-  // eslint-disable-next-line no-undef
-  const apiKey = __myapp.env.GOOGLE_MAP_API_KEY;
+let container;
+let map;
+// eslint-disable-next-line no-undef
+const apiKey = __myapp.env.GOOGLE_MAP_API_KEY;
 
-  let lat;
-  let lng;
+let lat;
+let lng;
 
-  onLocationChange((value) => {
-    lat = value.latitude;
-    lng = value.longitude;
+const makeUserLocation = () => {
+  // eslint-disable-next-line no-unused-vars
+  const marker = new google.maps.Marker({
+    map,
+    position: new google.maps.LatLng(lat, lng),
+    animation: google.maps.Animation.DROP,
+    icon: {
+      fillColor: '#FF0000', // 塗り潰し色
+      fillOpacity: 0.8, // 塗り潰し透過率
+      path: google.maps.SymbolPath.CIRCLE, // 円を指定
+      scale: 10, // 円のサイズ
+      strokeColor: '#FF0000', // 枠の色
+      strokeWeight: 1.0, // 枠の透過率
+    },
   });
+};
 
-  window.initMap = () => {
-    // eslint-disable-next-line no-undef
-    map = new google.maps.Map(container, {
-      zoom: 10,
-      center: { lat, lng },
+const makePlacePin = () => {
+  pins.map((place) => {
+    const marker = new google.maps.Marker({ // マーカーの追加
+      position: {
+        lat: place.latitude,
+        lng: place.longitude,
+      }, // マーカーを立てる位置を指定
+      map, // マーカーを立てる地図を指定
     });
-    console.log(map);
-  };
 
-  // onMount(async () => {
-  // });
+    const infoWindow = new google.maps.InfoWindow({ // 吹き出しの追加
+      content: `<div class="sample">${place.description}</div>`, // 吹き出しに表示する内容
+    });
+    marker.addListener('click', () => { // マーカーをクリックしたとき
+      infoWindow.open(map, marker); // 吹き出しの表示
+    });
+    return null;
+  });
+};
+onLocationChange((value) => {
+  lat = value.latitude;
+  lng = value.longitude;
+});
+
+window.initMap = () => {
+  // eslint-disable-next-line no-undef
+  map = new google.maps.Map(container, {
+    zoom: 15,
+    center: { lat, lng },
+  });
+  makeUserLocation();
+  makePlacePin();
+};
+
+// onMount(async () => {
+// });
 </script>
 <svelte:head>
   <script defer async
