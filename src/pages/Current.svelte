460e23ba307f71bcd06e9e54b21d4631d231d0cf
Fogrexon <fogrexon@gmail.com>
2020-09-15 20:09:43 +0900

    station

--- src/pages/Current.svelte
+++ src/pages/Current.svelte
@@ -1,10 +1,11 @@
 <script>
   import { Row, Col } from 'sveltestrap';
+  import StationList from '../components/Current/StationList.svelte';
 </script>
 <div class="wrapper">
   <Row>
     <Col xs="6" sm="4">
-      This is left column
+      <StationList />
     </Col>
     <Col xs="12" sm="8">
       This.is right column
