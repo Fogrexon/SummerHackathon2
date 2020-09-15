3a3353945e58380e0ba1caf40d9245897e991c3e
Fogrexon <fogrexon@gmail.com>
2020-09-15 19:28:50 +0900

    current

--- src/pages/Current.svelte
+++ src/pages/Current.svelte
@@ -1,6 +1,15 @@
 <script>
-
+  import { Row, Col } from 'sveltestrap';
 </script>
-<div>
-  
+<div class="wrapper">
+  <Row>
+    <Col xs="6" sm="4">
+      This is left column
+    </Col>
+    <Col xs="12" sm="8">
+      This.is right column
+    </Col>
+  </Row>
 </div>
+<style>
+</style>
