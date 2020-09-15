b08a0a93a02c370b30e61acba6c9bbb0d48983e2
Fogrexon <fogrexon@gmail.com>
2020-09-15 18:54:50 +0900

    curret

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
 	import Router from 'svelte-spa-router';
+	import NavBar from './components/NavBar.svelte';
 	import Home from './pages/Home.svelte';
 	import Apitest from './pages/Apitest.svelte';
-	import NavBar from './components/NavBar.svelte';
+	import Current from './pages/Current.svelte';
+
 	const routes = {
 	  '/': Home,
 	  '/api': Apitest,
+	  '/current': Current,
 	};
 </script>
 
