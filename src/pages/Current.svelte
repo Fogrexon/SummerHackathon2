3b6f00ad94b6eceb25c04b1ff7b1627ec9c03fe0
Fogrexon <fogrexon@gmail.com>
2020-09-15 21:16:30 +0900

    PlaceLabel

--- src/pages/Current.svelte
+++ src/pages/Current.svelte
@@ -1,13 +1,15 @@
 <script>
   import { Row, Col } from 'sveltestrap';
   import StationList from '../components/Current/StationList.svelte';
+  import PlaceLabel from '../components/Current/PlaceLabel.svelte';
 </script>
 <div class="wrapper">
+  <PlaceLabel />
   <Row>
-    <Col xs="6" sm="4">
+    <Col xs="12" md="4">
       <StationList />
     </Col>
-    <Col xs="12" sm="8">
+    <Col xs="12" md="8">
       This.is right column
     </Col>
   </Row>
