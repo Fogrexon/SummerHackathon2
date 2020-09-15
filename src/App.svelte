deb9ad95859845d424b6a2eba82f03908c2ab486
chikuwa-taso <leoleozawawa@gmail.com>
2020-09-15 16:35:15 +0900

    navbarの実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script>
 	import Router from 'svelte-spa-router';
 	import Home from './pages/Home.svelte';
-
+	import NavBar from './component/navbar.svelte'
 	const routes = {
 		'/': Home,
 	}
 </script>
 
 <header>
+	<NavBar {NavBar} />
 </header>
 <main>
 	<Router {routes} />
+	
 </main>
 <footer>
 </footer>
---
+++ src/component/NavBar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import {
+      Collapse,
+      Navbar,
+      NavbarToggler,
+      NavbarBrand,
+      Nav,
+      NavItem,
+      NavLink
+    } from 'sveltestrap';
+  
+    let isOpen = false;
+    const toggle = () => (isOpen = !isOpen);
+  </script>
+  
+  <Navbar color="light" light>
+    <NavbarBrand href="/" class="mr-auto">hacktrip</NavbarBrand>
+    <NavbarToggler on:click={toggle} className="mr-2" />
+    <Collapse {isOpen} navbar>
+      <Nav navbar>
+        <NavItem>
+          <NavLink href="#components/">Current Location</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/bestguy/sveltestrap">About</NavLink>
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </Navbar>
