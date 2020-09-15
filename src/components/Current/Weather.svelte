1ae8c09fd0a0fca94d800cea433b7c559b95def6
chikuwa-taso <leoleozawawa@gmail.com>
2020-09-15 22:21:11 +0900

    weather

--- src/components/Current/Weather.svelte
+++ src/components/Current/Weather.svelte
@@ -1,16 +1,32 @@
-<script lang="ts">
+<script>
   import { Media } from 'sveltestrap';
+  import { onMount } from 'svelte';
+  import { getCurrentWeather } from '../../api/openweathermap';
+  import { onLocationChange } from '../../stores/geoLocationStore';
+  
+  let CurrentWeather = [];
+  onMount(() => {
+    // このコンポーネントがマウントされたとき
+    onLocationChange((value) => {
+      // 座標情報が更新されたら(更新が入ってなくても初回に実行される)
+      if (value.latitude === 0 && value.longitude === 0) return;
+      getCurrentWeather(value.latitude, value.longitude)
+        .then((data) => {
+          CurrentWeather = data;
+        });
+    });
+  });
 </script>
-
 <Media>
   <Media left href="#">
     <Media
       object
       src="http://openweathermap.org/img/wn/10d@2x.png"
-      alt="weather image" />
+      alt="CurrentWeather image" />
   </Media>
   <Media body>
-    XX℃
+    <Media heading>XX℃</Media>
+    
   </Media>
 </Media>
 
