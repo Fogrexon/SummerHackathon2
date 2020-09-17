941434c723d8a4e90c2a824cddfe4c5015ac2145
chikuwa-taso <leoleozawawa@gmail.com>
2020-09-17 14:16:03 +0900

    edit tpopage

--- src/pages/TopPage.svelte
+++ src/pages/TopPage.svelte
@@ -11,4 +11,22 @@
   <p>
     This page is categorised into portalsite for planing.
   </p>
+  <p class="tips">
+    <strong>
+    You can see "where you are", "How's the weather like", "what can you eat & enjoy" on this page.
+    </strong>
+  </p>
+  
+  <strong class="justdoit">
+    JUST OPEN THE NAVIGATION BAR ON THE RIGHT TOP AND CLICK "current location"!!!!!!
+  </strong>
 </Container>
+
+<style>
+  .justdoit{
+    font-size: 40px;
+  }
+  .tips{
+    font-size: 25px;
+  }
+</style>
